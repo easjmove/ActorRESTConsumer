bf41166aad16ef1343760bb7f8767f1aa42672d8
move <move@zealand.dk>
2023-10-03 09:55:38 +0200

    Add project files.

---
+++ ActorRESTConsumer/ActorWorker.cs
@@ -0,0 +1,47 @@
+﻿using ActorRepositoryLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActorRESTConsumer
+{
+    public class ActorWorker
+    {
+        private const string URL = "http://localhost:5216/api/Actors";
+
+        public async Task<List<Actor>?> Get()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    
+                    HttpResponseMessage response =
+                       await client.GetAsync(URL);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        List<Actor>? actorList =
+                            await response.Content.
+                            ReadFromJsonAsync<List<Actor>>();
+                        return actorList;
+                    }
+                    else if (response.StatusCode ==
+                        System.Net.HttpStatusCode.BadRequest)
+                    {
+                        throw new ArgumentException("Serve sent bad request");
+                    }
+                    else { return null; }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception
+                        ("Something bad happended: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ ActorRESTConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ActorRepositoryLib;
+using ActorRESTConsumer;
+
+ActorWorker worker = new ActorWorker();
+List<Actor> actorList = worker.Get().Result;
+
+foreach (Actor actor in actorList)
+{
+    Console.WriteLine(actor.Name);
+}
